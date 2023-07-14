c9543c2eebcc395724eb0fed03f436f77b9bc735
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-07-14 17:13:34 +0800

    测试传值

--- python_test.py
+++ python_test.py
@@ -1,21 +1,26 @@
 def test_singer(var_singer):
-    print(id(var_singer))
+    print("变量进入函数初始地址为: " + str(id(var_singer)))
     var_singer = 0
-    print(id(var_singer))
-    print(var_singer)
-
+    print("变量在函数内经过函数重新赋值后为: " + str(var_singer))
+    print("变量在函数内经过函数重新赋值后地址为: " + str(id(var_singer)))
 
 def test_list(var_list):
     var_list[0] = 999
+    print("变量在函数内经过函数重新赋值后为: ",end='')
     print(var_list)
 
 v = 10
+print("变量在全局变量中初始地址为: " + str(id(v)))
 t_t = test_singer(v)
-print(v)
+print("变量在经过函数重新赋值后地址为: " + str(id(v)))
 
 v_l = [0, 2, 4, 6]
-t_l = test_list(v_l)
+print("变量初始列表为",end='')
 print(v_l)
-
+print("变量在全局变量中初始地址为: " + str(id(v_l)))
+t_l = test_list(v_l)
+print("变量在函数内经过函数重新赋值后外部变量为: ",end='')
+print(v_l)
+ 
 
 
