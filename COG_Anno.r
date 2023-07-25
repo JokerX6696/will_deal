8f9d2c0955412eac22faf5ee2b466765f449c7c8
JokerX6696 <313445042@qq.com>
2023-07-25 15:29:19 +0800

    COG 自动化功能注释 待完成

---
+++ COG_Anno.r
@@ -0,0 +1,21 @@
+# 安装和加载biomaRt包
+install.packages("biomaRt")
+library(biomaRt)
+
+# 连接到Ensembl数据库
+ensembl = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+# 获取COG功能注释
+cog_annotations <- getBM(attributes=c("ensembl_gene_id", "cog_id", "cog_description"),
+                         filters="ensembl_gene_id",
+                         values="ENSG00000157764", # 输入您感兴趣的基因的Ensembl ID
+                         mart=ensembl)
+
+# 打印COG功能注释结果
+print(cog_annotations)
+
+
+
+
+
+
